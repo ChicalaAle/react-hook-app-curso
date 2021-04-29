d146ae8baf820e6de15d131fba93045a2b9dcda8
Alejandro Chicala <chicalaale6@gmail.com>
2021-04-29 09:38:33 -0300

    Actualizado 29/04/2021 - 9:38 AM

--- src/components/04-useRef/RealExampleRef.js
+++ src/components/04-useRef/RealExampleRef.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { FetchWithCustomHooks } from '../03-useFetch/FetchWithCustomHooks';
 
 export const RealExampleRef = () => {
+    
+    const [show, setShow] = useState(false);
+    
     return (
         <div>
             <h1>Real example for Ref Hooks</h1>
+            { show && <FetchWithCustomHooks/> }
+            <button className="btn btn-primary mt-3" onClick={ () => {
+                setShow(!show);
+            }}>
+                Show/Hide
+            </button>
         </div>
     )
 }
